e3feae0b2e0e529031032eef0e831c96305b7b65
Ryan Araujo <ryanaraujo01112001@gmail.com>
2023-04-05 15:53:31 -0300

    Criado e removido atributo dinâmico do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -19,4 +19,9 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    ryan.sobrenome = 'Araujo'
+    print(ryan.sobrenome)
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(ryan.__dict__)
 
