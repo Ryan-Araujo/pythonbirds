316df774a640e7e149525dfaed6ed9df2efe0131
Ryan Araujo <ryanaraujo01112001@gmail.com>
2023-04-05 15:34:02 -0300

    Criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome, idade = 21):
+    def __init__(self,*filhos, nome, idade = 21):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
@@ -9,9 +10,13 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa('Ryan')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
+    ryan = Pessoa(nome='Ryan')
+    luciano = Pessoa(ryan, nome='luciano')
+    print(Pessoa.cumprimentar(ryan))
+    print(id(ryan))
+    print(ryan.cumprimentar())
+    print(ryan.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
